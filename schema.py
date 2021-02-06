7a2a4658785aee54167188d5c7acae814acdfc32
databaseAndweb <databaseweb2021@gmail.com>
2021-02-06 13:42:04 -0500

    new schema and query js

--- schema.py
+++ schema.py
@@ -1,234 +1,127 @@
 import sys
 import graphene
 import mysql.connector as mysql
-from flask import request, url_for, session
-import cgi
-
+from flask import request, url_for
 
 class Table(graphene.ObjectType):
-    #table = graphene.String()
-    table = graphene.List(graphene.String)
+    table = graphene.String()
     
 class Skeleton(graphene.ObjectType):
-    #skeleton = graphene.List(graphene.NonNull(graphene.String))
-    # skeleton = graphene.List(graphene.String)
-    rm = graphene.String()
-    nameAndtype = graphene.String()
+    table = graphene.String()
+    columns = graphene.List(graphene.String)
+    dataTypes = graphene.List(graphene.String)
     
+class Answer(graphene.ObjectType):
+    sqlquery = graphene.String()
+    columns = graphene.List(graphene.String)
+    dataTypes = graphene.List(graphene.String)
+    answers = graphene.List(graphene.List(graphene.String))
     
-
-class Building(graphene.ObjectType):
-    bcode = graphene.String()
-    bname = graphene.String()
-
-class Media(graphene.ObjectType):
-    mcode = graphene.String()
-    description = graphene.String()
-
-class Room(graphene.ObjectType):
-    bldg = graphene.String()
-    rnumber = graphene.String()
-    cap = graphene.Int()
-    layout = graphene.String()
-    rtype = graphene.String()
-    dept = graphene.String()
-    media = graphene.List(Media)
-
-class Senator(graphene.ObjectType):
-    lname = graphene.String()
-    fname = graphene.String()
-    birthday = graphene.Date()
-    gender = graphene.String()
-    state = graphene.String()
-    party = graphene.String()
-    url = graphene.String()
-    twitter = graphene.String()
-    facebook = graphene.String()
-    youtube = graphene.String()
-    
-
-class HRep(graphene.ObjectType):
-    lname = graphene.String()
-    fname = graphene.String()
-    birthday = graphene.Date()
-    gender = graphene.String()
-    state = graphene.String()
-    district = graphene.Int()
-    party = graphene.String()
-    url = graphene.String()
-    twitter = graphene.String()
-    facebook = graphene.String()
-    youtube = graphene.String()
-
-
 class Queries(graphene.ObjectType):
     
-    buildings = graphene.List(Building)
-    media = graphene.List(Media)
-    rooms = graphene.List(Room,building=graphene.String())
-    room = graphene.Field(Room,building=graphene.String(),
-                               rno=graphene.String())
-    roommedia = graphene.List(Media,building=graphene.String(),
-                                    rno=graphene.String())
-    roomscap = graphene.List(Room,cap_lower=graphene.Int(),
-                                  cap_upper=graphene.Int())
-
-    senators = graphene.List(Senator)
-    hreps= graphene.List(HRep)
-    #tnames2 = graphene.Field( args={'tnames':graphene.List(graphene.String)})
     login = graphene.List(Table, user=graphene.String(), pwd=graphene.String(), db=graphene.String())
     skeletons = graphene.List(Skeleton, tnames=graphene.List(graphene.String), user=graphene.String(), pwd=graphene.String(), db=graphene.String())
+    get_answers = graphene.List(Answer, tabPar=graphene.List(graphene.String), colPar=graphene.List(graphene.String),
+            condPar=graphene.String(), user=graphene.String(), pwd=graphene.String(), db=graphene.String())
+
     
+               
     def resolve_login(self, info, user,pwd,db):
 
-        dbase = mysql.connect(
-        host="localhost",
-        database="qbe",
-        user="qbe",
-        passwd="q123"
-        #auth_plugin='mysql_native_password')
-        )
+        try:
 
-        if user =='qbe' and db == 'qbe' and pwd == 'q123':
-            query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'qbe' "            
+            dbase = mysql.connect(
+            host="localhost",
+            database=db,
+            user=user,
+            passwd=pwd
+            #auth_plugin='mysql_native_password')
+            )
+
+            query = "SELECT table_name FROM information_schema.tables WHERE table_schema = '"+db+"' "            
             cursor = dbase.cursor()
             cursor.execute(query)
             records = cursor.fetchall()
             cursor.close()
             dbase.close()
-            #if len(records) == 0:
-            #  return ??
             tables = []
             for record in records:
                 tables.append(Table(table=record))
         
-            #tables = [item for sublist in tables for item in sublist]
-        
             return tables
+        except:
+            dbase.rollback()
+            cursor.close()
+            dbase.close()
+            return []
         
     
     def resolve_skeletons(self, info,tnames,user,pwd,db):
 
-        if user =='qbe' and db == 'qbe' and pwd == 'q123':
-            
+        try:
 
-
-            db = mysql.connect(
+            dbase = mysql.connect(
                 host="localhost",
-                database="qbe",
-                user="qbe",
-                passwd="q123"
+                database=db,
+                user=user,
+                passwd=pwd
                 #auth_plugin='mysql_native_password'
             )
-            # if user =='qbe' and db == 'qbe' and pwd == 'q123':
-                # query = "SELECT column_name, data_type FROM information_schema.columns WHERE TABLE_NAME = @nm and table_schema = 'qbe' " 
-            query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'qbe' "            
-            cursor = db.cursor()
-            cursor.execute(query)
-            records = cursor.fetchall()
-            records = [x[0] for x in records]
-            
-            for i in records:
-                tnames.append(i)
 
-            bbb = []
-            #item = 'BUILDING'
+            result = []
             
             for item in tnames:
-                if item in records:
+                query = "SELECT t.table_name,  sc.column_name, sc.data_type FROM information_schema.tables t INNER JOIN " + \
+                "information_schema.columns sc ON t.table_schema=sc.table_schema AND t.TABLE_NAME = sc.TABLE_NAME WHERE t.table_schema = 'qbe' and " + \
+                "t.TABLE_NAME = '"+item+"' " 
+                cursor = dbase.cursor()
+                cursor.execute(query)
+                records = cursor.fetchall()
+                print(records)
 
-                    query2 = "SELECT t.table_name,  sc.column_name, sc.data_type FROM information_schema.tables t INNER JOIN " + \
-                    "information_schema.columns sc ON t.table_schema=sc.table_schema AND t.TABLE_NAME = sc.TABLE_NAME WHERE t.table_schema = 'qbe' and " + \
-                    "t.TABLE_NAME = '"+item+"' " 
-                    cursor = db.cursor()
-                    cursor.execute(query2)
-                    records2 = cursor.fetchall()
-                    #print(records2)
-                    bbb.append(records2)
-                    break
-                else:
-                    return None
+                cols = []
+                dtypes = []
 
-            
-            #list1 = list(set([x[0] for x in bbb]))
-            #print(list1)
-            list2 = [el[1:] for el in records2]
-            #print(list2)
-            
+                for record in records:
+                    cols.append(record[1])
+                    dtypes.append(record[2])
+                print(item, cols, dtypes)
+                result.append(Skeleton(table=item, columns=cols, dataTypes=dtypes))
             cursor.close()
-            db.close()
-            skeletons2 = []
-            listChars = []
-            
-            for field, value in list2:
-                res = '{}({})'.format(field,value)
-                listChars.append(res)
-            print(listChars)
-            
-            final = listChars
-            print(final)
-            
-            skeletons2 = [Skeleton(nameAndtype=element) for element in final]
-            # for element in final:
-            #     skeletons2.append(Skeleton(nameAndtype=element))
-
-            
-            
-            return skeletons2
-        else:
-            return None
-        
+            dbase.close()
+            return result
+        except:
+            dbase.rollback()
+            cursor.close()
+            dbase.close()
+            return []
+                
+    def resolve_get_answers(self, info, tabPar, colPar,condPar, user, pwd, db):
 
 
+        try:
+            
 
-    def resolve_senators(self, info):
-        db = mysql.connect(
-            host="localhost",
-            database="qbe",
-            user="qbe",
-            passwd="q123"
-            #auth_plugin='mysql_native_password'
-        )
-        query = "select lname, fname, birthday, gender, state, "+ \
-                 "party, url, twitter, facebook, youtube  from SENATOR "            
-        cursor = db.cursor()
-        cursor.execute(query)
-        records = cursor.fetchall()
-        cursor.close()
-        db.close()
-        #if len(records) == 0:
-        #  return ??
-        senators = []
-        for record in records:
-            senators.append(Senator(lname=record[0], fname=record[1], birthday=record[2], gender=record[3],
-             state=record[4], party=record[5], url=record[6], twitter=record[7], facebook=record[8], youtube=record[9]))
-        return senators
-    
-    def resolve_hreps(self, info):
-        db = mysql.connect(
-            host="localhost",
-            database="qbe",
-            user="qbe",
-            passwd="q123"
-            #auth_plugin='mysql_native_password'
-        )
-        query = "select lname, fname, birthday, gender, state, "+ \
-                 "district, party, url, twitter, facebook, youtube  from HREP "            
-        cursor = db.cursor()
-        cursor.execute(query)
-        records = cursor.fetchall()
-        cursor.close()
-        db.close()
-        #if len(records) == 0:
-        #  return ??
-        hreps = []
-        for record in records:
-            hreps.append(HRep(lname=record[0], fname=record[1], birthday=record[2], gender=record[3],
-             state=record[4], district=record[5], party=record[6], url=record[7], twitter=record[8], facebook=record[9], youtube=record[10]))
-        return hreps
-    
-    
 
+            dbase = mysql.connect(
+                host="localhost",
+                database=db,
+                user=user,
+                passwd=pwd
+                #auth_plugin='mysql_native_password'
+            )
+
+            
+            print("nhgh hkhk", tabPar)
+            print(colPar)
+            print(condPar)
+
+        except:
+            return []
+            
+            
+            
+            
     
 schema = graphene.Schema(query=Queries)
 
@@ -236,4 +129,6 @@ schema = graphene.Schema(query=Queries)
 tt = Skeleton(graphene.ObjectType)
 aa = Queries(graphene.ObjectType)
 ab = []
-print(aa.resolve_skeletons(tt,ab, "qbe", "q123", "qbe"))
+bb = []
+#print(aa.resolve_get_answers(tt,ab,bb, "yy", "qbe", "q123", "qbe"))
+#print(aa.resolve_skeletons(tt, ab, "qbe", "q123", "qbe"))
--- static/query.js
+++ static/query.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     
     $("#form1").on('submit', function(e){
         e.preventDefault();
+        
         // const user = $("#user").val();
         // const pwd = $("#pwd").val();
         // const db = $("#db").val();
@@ -29,19 +30,31 @@ $(document).ready(function(){
         type:'POST',
         data: login,
         success: function(result){
-            data = result.data.login;        
-            var htmlCode = "<table>";
+            data = result.data.login;
+            console.log(data)       
+            var htmlCode = "<table><tr>";
             arr = [];
+            console.log(data);
             for(var i = 0; i < data.length; i++){
                 var item = Object.values(data[i]);
                 //arr.push(item);
-                //console.log(arr);
+                console.log(item);
                 
-                 htmlCode+= '<tr><td><b>'+item+":"+'</b></td><td><select><option value="0">0</option><option value="1">1</option>' +
-                 '<option value="2">2</option></select></td></tr>';
+                 htmlCode+= '<td><b>'+item+":"+'</b></td>'
+                     htmlCode +='<td><select id="tnames" onchange="populateTables()">'+
+                    '<option id="None" value="0">0</option>'; 
+                    
+                    for (var j = 1; j<  3; j++){
+                        htmlCode+='<option value="'+item+'">'+j+'</option>';
+                        
+                        
+                    }
+
+                    htmlCode +=  "</td></select></tr><table>";
+                    
             }
-            htmlCode += "<table>";
-            htmlCode +='<input type="button" value="Get Skeletons" onclick="getSkel()"><input type="reset" value="Reset Skeletons">'
+            //htmlCode += "</select></td></tr><table>";
+            htmlCode +='<input type="button" value="Get Skeletons" onclick="populateTables()"><input type="reset" value="Reset Skeletons">'
             $("#showDatabaseTableForm").html(htmlCode);
         },
         error: function(error){
@@ -53,35 +66,41 @@ $(document).ready(function(){
 });
 
 
-function getSkel() {
-    console.log("hgjghjjjjjjjjjjjjjjjg")
-
-    // var text = document.getElementById("interface");
-    // if (text.style.display === "none"){
-    //     text.style.display = "block";
-    // } else{
-    //     text.style.display = "none";
-    // }
+function populateTables() {
+    console.log("hgjghjjjjjjjjjjjjjjjg");
+    //An example :
+    //var tnames = $("#dropdwn").children("option:selected").text(); 
+    //var tnames = $("#dropdwn").children("option:selected").val();
+    //console.log(tnames);
+    //var tnames = "ROOM";
+    
+    
     var url = 'http://127.0.0.1:5000/qbe/?query={skeletons(tnames:["'+$("#tnames").val()+'"],user:"'+$("#user").val()+ '",pwd:"'+
-    $("#pwd").val()+ '",db:"'+$("#db").val()+ '"){nameAndtype}}'
+    $("#pwd").val()+ '",db:"'+$("#db").val()+ '"){table colAndtype}}'
     $.ajax({
       url: url,
-      type: 'POST',
+      type: 'GET',
       success: function(response) {
         var skel = response.data.skeletons;
-        var htmlCode = '<table class="borderedtables" style="width: 100%;">'+
-                   '<tr class="tableheader"><tr class="tableheader">'
+        newTable = [];
+        for(var j = 0; j < skel.length; j++){
+            newTable.push(skel[j].table);
+
+        }
+        var htmlCode = '<table class="borderedtables">'+
+                   '<tr class="tableheader"><tr class="tableheader">'+
+                   '<td class="borderedtables">'+newTable[0]+'</td>';
         for (var i=0; i<skel.length; i++){
-            var item = Object.values(skel[i]);
-            htmlCode += '<td class="borderedtables">'+item+'</td>'
+            //var item = Object.values(skel[i]);
+            htmlCode += '<td class="borderedtables">'+skel[i].colAndtype+'</td>';
             // htmlCode+='<tr class=""><td><input type="text"></td>test</td></tr>';
         }
-        htmlCode +='</tr><tr class="borderedtables">'
-        for(var i=0; i<skel.length; i++){
-            htmlCode+='<td><input type="text" size="24" width=200px></td>'
+        htmlCode +='</tr><tr class="borderedtables">';
+        for(var i=0; i<skel.length+1; i++){
+            htmlCode+='<td><input type="text"></td>';
         }
 
-        htmlCode += '</tr></table>'
+        htmlCode += '</tr></table>';
         $("#interface").html(htmlCode);
       },
       error: function(error) {
@@ -91,3 +110,18 @@ function getSkel() {
     });
   };
   
+  $(document).ready(function(){
+
+    $("#lastPart").on('submit', function(e){
+        e.preventDefault();
+
+        const tabPar = $("#user").val();
+        const colPar = $("#pwd").val();
+        const condPar = $("#db").val();
+
+
+    })
+
+    
+
+ });
